dcaed5d3d9a9f3ab1ed20a5481a3516d45c8f9a9
jiheyon <dkgus5988@gmail.com>
2022-11-28 23:06:14 +0900

    error

---
+++ src/main/java/com/example/demo/searchNeed.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class searchNeed {
+
+    /*
+    @ToString
+    @RestController
+//@Controller
+//@ResponseBody
+  @GetMapping("/china")
+
+    List<String> foodList
+                = Arrays.asList("마파두부", "꿔바로우", "마라탕");
+  @AllArgsConstructor
+
+  @RestController
+@RequestMapping("/json")
+@Slf4j // 로깅 라이브러리
+
+    */
+}
